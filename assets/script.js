117001a5806a46a23c545e4e29e58a746de255f0
Daniel <ttdanny98@gmail.com>
2023-02-07 14:11:22 -0800

    added weather API to the 5 day forecast containers

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,42 @@
-var date = dayjs().format('MM/DD/YYYY');
-var searchBtn = document.querySelector("#search-btn");
-var cityInput = document.querySelector("#city-search");
-var cityResults = document.querySelector(".result-btns");
-var cityNameText = document.querySelector("#cityName-Text");
-var weatherTemptext= document.querySelector("#weatherTemp-text");
-var humidityText = document.querySelector("#humidity-text");
-var windSpeedText = document.querySelector("#windspeed-text");
-var cityIcon = document.querySelector("#icon");
-var forecastBox = document.querySelectorAll(".forecast-box");
-var forecast1 = document.querySelector("#forecast1");
-var forecast2 = document.querySelector("#forecast2");
-var forecast3 = document.querySelector("#forecast3");
-var forecast4 = document.querySelector("#forecast4");
-var forecast5 = document.querySelector("#forecast5");
+const date = dayjs().format('MM/DD/YYYY');
+const searchBtn = document.querySelector("#search-btn");
+const cityInput = document.querySelector("#city-search");
+const cityResults = document.querySelector(".result-btns");
+const cityNameText = document.querySelector("#cityName-Text");
+const weatherTemptext= document.querySelector("#weatherTemp-text");
+const humidityText = document.querySelector("#humidity-text");
+const windSpeedText = document.querySelector("#windspeed-text");
+const cityIcon = document.querySelector("#icon");
+const forecastBox = [];
+const forecastDate = [];
+const forecastTemps = [];
+const forecastWindspeeds = [];
+const forecastHumidities = [];
+forecastBox.push(document.getElementById("forecast1"));
+forecastBox.push(document.getElementById("forecast2"));
+forecastBox.push(document.getElementById("forecast3"));
+forecastBox.push(document.getElementById("forecast4"));
+forecastBox.push(document.getElementById("forecast5"));
+forecastDate.push(document.getElementById('forecast-date-1'));
+forecastDate.push(document.getElementById('forecast-date-2'));
+forecastDate.push(document.getElementById('forecast-date-3'));
+forecastDate.push(document.getElementById('forecast-date-4'));
+forecastDate.push(document.getElementById('forecast-date-5'));
+forecastTemps.push(document.getElementById('forecast-temp-1'));
+forecastTemps.push(document.getElementById('forecast-temp-2'));
+forecastTemps.push(document.getElementById('forecast-temp-3'));
+forecastTemps.push(document.getElementById('forecast-temp-4'));
+forecastTemps.push(document.getElementById('forecast-temp-5'));
+forecastHumidities.push(document.getElementById('forecast-humidity-1'));
+forecastHumidities.push(document.getElementById('forecast-humidity-2'));
+forecastHumidities.push(document.getElementById('forecast-humidity-3'));
+forecastHumidities.push(document.getElementById('forecast-humidity-4'));
+forecastHumidities.push(document.getElementById('forecast-humidity-5'));
+forecastWindspeeds.push(document.getElementById('forecast-windSpeed-1'));
+forecastWindspeeds.push(document.getElementById('forecast-windSpeed-2'));
+forecastWindspeeds.push(document.getElementById('forecast-windSpeed-3'));
+forecastWindspeeds.push(document.getElementById('forecast-windSpeed-4'));
+forecastWindspeeds.push(document.getElementById('forecast-windSpeed-5'));
 
 
 
@@ -33,7 +57,7 @@ var weatherAPI = function (){
         alert("Not a valid city")
     };
 }) .then (function(data) {
-    console.log(data)
+    // console.log(data)
     const { name } = data;
     const { icon } = data.weather[0];
     const { temp } = data.main;
@@ -44,7 +68,7 @@ var weatherAPI = function (){
     weatherTemptext.innerHTML = "Temperature: " + temp + "°F";
     humidityText.innerHTML = "Humidity: " + humidity + " %";
     windSpeedText.innerHTML = "Wind: " + speed + " MPH";  
-    cityIcon.src = "http://openweathermap.org/img/wn/" + icon + ".png";
+    cityIcon.src = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 });
 // Fetches forecast for 5 days
 fetch('https://api.openweathermap.org/data/2.5/forecast?q='+cityInput.value+'&units=imperial&appid=6b2fe1f5e9c799498a3cb8dcfcab7b18')
@@ -55,12 +79,21 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?q='+cityInput.value+'&un
     console.log(data);
 
 
-    
 
-})
+var fcIndex = 0;
+for(i = 0; i<data.list.length; i=i+8){
+    forecastBox[fcIndex].style.display = "block";
+     forecastDate[fcIndex].innerHTML = dayjs(data.list[i].dt_txt).format('MM/DD/YYYY');
+     forecastTemps[fcIndex].innerHTML = data.list[i].main.temp + "°F";
+     forecastHumidities[fcIndex].innerHTML = data.list[i].main.humidity + "%";
+     forecastWindspeeds[fcIndex].innerHTML = data.list[i].wind.speed + " MPH";
 
-
+    fcIndex++;
 };
+});
+};
+
+
 
 // Pressing on search button will add a button that has the city's name.
 function addCityList() {
@@ -73,6 +106,7 @@ function addCityList() {
     
     cityInput.value = "";
 
+
 };
 
 
@@ -82,7 +116,6 @@ function addCityList() {
 
 searchBtn.addEventListener("click", weatherAPI);
 searchBtn.addEventListener("click", addCityList);
-
 day();
 
 
